0be1f1609a1871420b52260669e7ba1b47b13678
Shibkov-Gleb <shibkovgleb@gmail.com>
2026-08-29 16:51:13 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import os
+from pathlib import Path
+
+import chromadb
+from dotenv import load_dotenv
+from llama_index.core import Document, StorageContext, VectorStoreIndex
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.llms.openai import OpenAI
+from llama_index.vector_stores.chroma import ChromaVectorStore
